4df2013f240a7bca7167a024d15d4e719b8a64b2
Eben <eben@glucode.com>
2016-03-10 11:17:37 +0200

    Remove ViewController and Storyboards because we do not need it for the kata

--- GildedRose/main.m
+++ GildedRose/main.m
@@ -1,15 +1,5 @@
-//
-//  main.m
-//  GildedRose
-//
-//  Created by amok on 2016/03/10.
-//  Copyright (c) 2016 martincronje. All rights reserved.
-//
-
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
