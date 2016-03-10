80e0b07174c71c1cef840792c5490ef80dda12ce
Eben <eben@glucode.com>
2016-03-10 11:15:30 +0200

    Cleanup test class to make the kata easier to get going

--- GildedRoseTests/GildedRoseTests.m
+++ GildedRoseTests/GildedRoseTests.m
@@ -6,7 +6,9 @@
 @end
 
 @implementation GildedRoseTests
-/*
+
+-(void) testSample {
+
     NSArray *items = @[
             [Item itemWithName:@"+5 Dexterity Vest" sellIn:10 andQuality:20],
             [Item itemWithName:@"Aged Brie" sellIn:2 andQuality:0],
@@ -20,26 +22,7 @@
 
     [gildedRose updateQuality];
 
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-*/
-
-
--(void) testFoo {
-
-    NSArray *items = @[
-            [Item itemWithName:@"+5 Dexterity Vest" sellIn:10 andQuality:20],
-         //   [Item itemWithName:@"Aged Brie" sellIn:2 andQuality:0],
-          //  [Item itemWithName:@"Elixir of the Mongoose" sellIn:5 andQuality:7],
-          //  [Item itemWithName:@"Sulfuras, Hand of Ragnaros" sellIn:0 andQuality:80],
-            //[Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:15 andQuality:20],
-          //  [Item itemWithName:@"Conjured Mana Cake" sellIn:3 andQuality:6]
-    ];
-
-    GildedRose *gildedRose = [[GildedRose alloc] initWith:items];
-
-    [gildedRose updateQuality];
-
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTFail(@"Implement your own");
 }
 
 @end
