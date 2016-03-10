ed6a9ffd95326c4e2adb226ba662dc3fc996103c
Eben <eben@glucode.com>
2016-03-10 11:33:33 +0200

    Cleanup

---
+++ GildedRose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GildedRose
+//
+//  Created by Eben Kok on 2016/03/10.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
