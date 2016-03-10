c760efd32db0e2a0f744c1166e89dc4db52f7de7
Eben <eben@glucode.com>
2016-03-10 12:01:45 +0200

    Code coverage on Gilded files 100%

--- GildedRoseTests/GildedRoseTests.m
+++ GildedRoseTests/GildedRoseTests.m
@@ -7,22 +7,121 @@
 
 @implementation GildedRoseTests
 
--(void) testSample {
+- (void)testThatQualityDecreasesTwiceAsFastWhenSellByDateHasPassed {
+    Item *item = [Item itemWithName:@"+5 Dexterity Vest" sellIn:1 andQuality:20];
 
-    NSArray *items = @[
-            [Item itemWithName:@"+5 Dexterity Vest" sellIn:10 andQuality:20],
-            [Item itemWithName:@"Aged Brie" sellIn:2 andQuality:0],
-            [Item itemWithName:@"Elixir of the Mongoose" sellIn:5 andQuality:7],
-            [Item itemWithName:@"Sulfuras, Hand of Ragnaros" sellIn:0 andQuality:80],
-            [Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:15 andQuality:20],
-            [Item itemWithName:@"Conjured Mana Cake" sellIn:3 andQuality:6]
-    ];
-
-    GildedRose *gildedRose = [[GildedRose alloc] initWith:items];
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
 
     [gildedRose updateQuality];
 
-    XCTFail(@"Implement your own");
+    XCTAssertEqual(19, item.quality);
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(17, item.quality);
 }
+
+- (void)testThatTheQualityOfAnItemIsNeverNegative {
+    Item *item = [Item itemWithName:@"+5 Dexterity Vest" sellIn:1 andQuality:0];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(0, item.quality);
+}
+
+- (void)testThatBrieIncreasesInQualityWithAge {
+    Item *item = [Item itemWithName:@"Aged Brie" sellIn:1 andQuality:0];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(1, item.quality);
+}
+
+- (void)testThatBrieIncreasesTwiceAsFastAfterSellInExpired {
+    Item *item = [Item itemWithName:@"Aged Brie" sellIn:0 andQuality:0];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(2, item.quality);
+}
+
+- (void)testThatQualityOfAnItemIsNeverMoreThan50 {
+    Item *item = [Item itemWithName:@"Aged Brie" sellIn:50 andQuality:50];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(50, item.quality);
+}
+
+- (void)testThatSulfurasQualityDoesntDecrease {
+    Item *item = [Item itemWithName:@"Sulfuras, Hand of Ragnaros" sellIn:0 andQuality:80];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(80, item.quality);
+}
+
+- (void)testThatSulfurasSellInDoesntDecrease {
+    Item *item = [Item itemWithName:@"Sulfuras, Hand of Ragnaros" sellIn:1 andQuality:80];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(1, item.sellIn);
+}
+
+- (void)testThatBackstagePassesQualityIncreasesBy1WhenSellInOver10 {
+    Item *item = [Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:15 andQuality:20];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(21, item.quality);
+}
+
+- (void)testThatBackstagePassesQualityIncreasesBy2WhenSellInOver5AndLessThanEqual10 {
+    Item *item = [Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:10 andQuality:20];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(22, item.quality);
+}
+
+- (void)testThatBackstagePassesQualityIncreasesBy3WhenSellInLessThanEqual5 {
+    Item *item = [Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:5 andQuality:20];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(23, item.quality);
+}
+
+- (void)testThatBackstagePassesQualityIsZeroWhenSellInZero {
+    Item *item = [Item itemWithName:@"Backstage passes to a TAFKAL80ETC concert" sellIn:0 andQuality:20];
+
+    GildedRose *gildedRose = [[GildedRose alloc] initWith:@[item]];
+
+    [gildedRose updateQuality];
+
+    XCTAssertEqual(0, item.quality);
+}
+
+
+
 
 @end
